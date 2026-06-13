0c5406ab64e56788ea7006925b2f404e0c6298de
ciddy0 <96404041+ciddy0@users.noreply.github.com>
2026-06-12 17:10:36 -0700

    Update vite.config.ts

--- new-frontend/vite.config.ts
+++ new-frontend/vite.config.ts
@@ -11,8 +11,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+        manualChunks(id) {
+          if (id.includes('node_modules/react-dom') ||
+              id.includes('node_modules/react-router-dom') ||
+              id.includes('node_modules/react/')) {
+            return 'vendor';
+          }
         },
       },
     },
